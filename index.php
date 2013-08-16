d9cc0d7f7b217aa58a886e589080e12268fee78d
Xiao Lv <lvtongda@gmail.com>
2013-08-16 17:21:05 +0800

    Deploy the UEditor1.2.6.1 PHP version

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <title>UpYun with UEditor</title>
+        <script type="text/javascript" src="ueditor.config.js"></script>
+        <script type="text/javascript" src="ueditor.all.min.js"></script>
+    </head>
+    <textarea name="后台取值的key" id="myEditor">这里写你的初始化内容</textarea>
+    <script>
+        var editor = new UE.ui.Editor();
+        editor.render("myEditor");
+    </script>
+</html>
