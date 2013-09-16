e264b7b0671d80c52da2173eaa6917da8927dbdc
Xiao Lv <lvtongda@gmail.com>
2013-09-16 16:45:17 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
@@ -5,7 +6,9 @@
         <script type="text/javascript" src="ueditor.config.js"></script>
         <script type="text/javascript" src="ueditor.all.min.js"></script>
     </head>
-    <textarea name="后台取值的key" id="myEditor">这里写你的初始化内容</textarea>
+    <body>
+        <textarea name="后台取值的key" id="myEditor">这里写你的初始化内容</textarea>
+    </body>
     <script>
         var editor = new UE.ui.Editor();
         editor.render("myEditor");
